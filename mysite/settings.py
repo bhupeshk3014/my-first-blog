2b1cf1866a04ff1a3a14f28add8105029ebae46f
bhupesh <bhupeshkumar003@gmail.com>
2018-06-25 13:06:18 +0530

    Changed the title.

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -119,3 +118,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
